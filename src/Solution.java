2b66b4ce5cf0b6dc589422d5346d0c768f4f3353
Kirill Temnenkov <kirill@temnenkov.com>
2024-04-21 12:12:36 +0300

    first version

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.io.*;
 import java.math.*;
+import java.util.stream.Collectors;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -9,29 +10,79 @@ import java.math.*;
 class Solution {
 
     public static void main(String args[]) {
+        final Turing solver = new Turing();
+
         Scanner in = new Scanner(System.in);
         int s = in.nextInt();
         int t = in.nextInt();
+        solver.tape = new int[t];
         int x = in.nextInt();
+        solver.pos = x;
         if (in.hasNextLine()) {
             in.nextLine();
         }
         String start = in.nextLine();
+        solver.state = start;
         int n = in.nextInt();
         if (in.hasNextLine()) {
             in.nextLine();
         }
         for (int i = 0; i < n; i++) {
             String stateActions = in.nextLine();
+            final String[] byDv = stateActions.split(":");
+            final ArrayList<Turing.Instruction> instructionArrayList = new ArrayList<>();
+            solver.instructions.put(byDv[0], instructionArrayList);
+            final String[] bySemicolon = byDv[1].split(";");
+            Arrays.stream(bySemicolon).sequential().forEach(it -> {
+                final String[] tokens = it.split(" ");
+                instructionArrayList.add(new Turing.Instruction(Integer.parseInt(tokens[0]), "L".equals(tokens[1]), tokens[2]));
+            });
         }
 
-        System.out.println("answer");
+        solver.run();
+        System.out.println(solver.counter);
+        System.out.println(solver.pos);
+        System.out.println(Arrays.stream(solver.tape)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining("")));
     }
 }
 
 class Turing {
-    private final Map<String, List<Instruction>> instructions = new HashMap<>();
+    final Map<String, List<Instruction>> instructions = new HashMap<>();
+    String state;
+    int[] tape;
+    int pos;
+    int counter = 0;
 
-    private record Instruction(int symbolToWrite, boolean moveLeft, String nextState) {
+    record Instruction(int symbolToWrite, boolean moveLeft, String nextState) {
+    }
+
+    void run() {
+
+
+        while(true) {
+            ++counter;
+            int currSymbol = tape[pos];
+            Instruction instruction = instructions.get(state).get(currSymbol);
+            tape[pos] = instruction.symbolToWrite;
+            int newPos = pos;
+            if (instruction.moveLeft) {
+                --newPos;
+            } else {
+                ++newPos;
+            }
+            if (newPos < 0 || newPos >= tape.length) {
+                return;
+            }
+            pos = newPos;
+
+            if (instruction.nextState.equals("HALT")) {
+                return;
+            }
+
+            state = instruction.nextState;
+        }
+
     }
 }
